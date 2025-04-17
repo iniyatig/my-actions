b6d8400d933dd061dccedb2248ef08390964256d
Niyati Gada <145377465+galaniyati@users.noreply.github.com>
2025-04-17 19:54:52 +0530

    Update get_loc_by_language.sh

--- get_loc_by_language.sh
+++ get_loc_by_language.sh
@@ -9,7 +9,7 @@ git log --author="$author_name" --pretty=tformat: --numstat --since="1970-01-01T
 awk '
   NF == 3 { added += $1; deleted += $2; split($3, parts, "."); if (length(parts) > 1) { lang = parts[length(parts)]; lang_added[lang] += $1; lang_deleted[lang] += $2 } }
   END {
-    printf "language_loc:"
+    printf "language_loc:\n";
     for (l in lang_added) {
       loc = lang_added[l] - lang_deleted[l]
       if (loc != 0) {
